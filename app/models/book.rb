3a40b220c8ea463585c1c1afba17ba0d4b0ba873
EC2 Default User <ec2-user@ip-172-31-29-54.ec2.internal>
2022-05-24 21:44:55 +0000

    FirstCommit

---
+++ app/models/book.rb
@@ -0,0 +1,23 @@
+class Book < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  validates :title, presence:true
+  validates :body, presence:true, length:{ maximum: 200 }
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+
+  patch "books/:id" => "books#update", as: "update_book"
+  patch "users/:id" => "users#update", as: "update_user"
+
+  resources :homes, only: [:top, :about]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
